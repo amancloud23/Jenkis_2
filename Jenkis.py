11d60ee7f23df5df308421b0dfd411d14af39d32
Aman Patwa <O24MCA110682@cuchd.in>
2025-07-13 22:02:51 +0530

    Update Jenkis.py

--- Jenkis.py
+++ Jenkis.py
@@ -1,2 +1,2 @@
 print ("Hello Aman The code is working now after chengin the Vesrsion 1.2.")
-print ("Now we are checking the Autobuild")
+print ("Now we are checking the Autobuild is working on 13 June")
